c4eca8d17761300bed2765871561ea06479d947f
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:56 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for build-a-fraction.
+ * ESLint configuration for build-a-fraction.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
