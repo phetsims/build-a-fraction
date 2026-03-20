687f4a367db31a25e210720778d64baaf575d11b
phet-dev <phetadmin@gmail.com>
2026-03-20 13:47:25 -0600

    grunt update  from daily grunt work

--- js/BuildAFractionStrings.ts
+++ js/BuildAFractionStrings.ts
@@ -28,4 +28,6 @@ type StringsType = {
 
 const BuildAFractionStrings = getStringModule( 'BUILD_A_FRACTION' ) as StringsType;
 
+buildAFraction.register( 'BuildAFractionStrings', BuildAFractionStrings );
+
 export default BuildAFractionStrings;
